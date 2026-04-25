fcf9dd06b67ed054d82718dbc0d104f459d09f13
thetwelvedev <twelve.programming@gmai.com>
2026-04-25 17:34:52 -0400

    Código atualizado e gerado o executável

--- code/FazAlgo.c
+++ code/FazAlgo.c
@@ -11,7 +11,7 @@ void FazAlgo(int n){
     int i,j,k;
 
     for(i=1;i<n-1;i++){
-        for(j=2;j<=n;j++){
+        for(j=i+1;j<=n;j++){
             for(k=1;k<=j;k++){
 
                 soma +=1;
